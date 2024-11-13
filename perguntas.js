bf6db42cb959a88307878dfe31fbc2c7e9ac283a
luana <0000110157625xsp@al.educacao.sp.gov.br>
2024-11-13 08:40:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Literatura',
+    'Quem escreveu o livro O Príncipe?',
+    'Maquiavel'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos continentes existem?',
+    '6'
+)
+
+criaCartao(
+    'Arte',
+    'Quem pintou a Mona Lisa?',
+    'Leonardo da Vinci'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz pato em Inglês?',
+    'Duck'
+)
